aa108258c91c90ef53fca62df369cb7f1cbe88ce
Richard Schmidt <richardnew12511@gmail.com>
2024-08-08 14:05:41 -0500

    refactor telescope to plugins folder

--- init.lua
+++ init.lua
@@ -36,7 +36,7 @@ require("lazy").setup({
   spec = {
 		{ import = "plugins" },
     -- add your plugins here
-    { 'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }},
+--nvim    { 'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }},
     {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
 		{
 			"nvim-neo-tree/neo-tree.nvim",
@@ -58,11 +58,11 @@ require("lazy").setup({
 
 
 -- Setup Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+--local builtin = require('telescope.builtin')
+--vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+--vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+--vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+--vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 -- Setup Treesitter
 local configs = require("nvim-treesitter.configs")
