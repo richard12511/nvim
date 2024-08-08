913057b5e1279ca3160d19e9c4d95f5f041fa401
Richard Schmidt <richardnew12511@gmail.com>
2024-08-08 14:20:46 -0500

    move rest of plugins to plugins dir

--- init.lua
+++ init.lua
@@ -37,17 +37,6 @@ require("lazy").setup({
 		{ import = "plugins" },
     -- add your plugins here
 --nvim    { 'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }},
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-		{
-			"nvim-neo-tree/neo-tree.nvim",
-			branch = "v3.x",
-			dependencies = {
-				"nvim-lua/plenary.nvim",
-				"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-				"MunifTanjim/nui.nvim",
-				-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-			}
-		}
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
@@ -57,21 +46,5 @@ require("lazy").setup({
 })
 
 
--- Setup Telescope
---local builtin = require('telescope.builtin')
---vim.keymap.set('n', '<C-p>', builtin.find_files, {})
---vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
---vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
---vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
 -- Setup Treesitter
-local configs = require("nvim-treesitter.configs")
-configs.setup({
-	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-      	sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },  
-})
-
 -- Setup Neotree
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
