9985d97cfb255dc734b8cff39666d4177a2b4b0e
Richard Schmidt <richardnew12511@gmail.com>
2024-08-08 10:24:13 -0500

    remap telescope keys to default

--- init.lua
+++ init.lua
@@ -51,7 +51,7 @@ vim.cmd.colorscheme("catppuccin")
 -- Setup Telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
-vim.keymap.set('n', '<C-b>', builtin.buffers, {})
-vim.keymap.set('n', '<C-h>', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
