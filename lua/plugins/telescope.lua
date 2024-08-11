829e5a036b1a7ad42e3dd4c1cd784032554ce52d
Richard Schmidt <richardnew12511@gmail.com>
2024-08-11 14:49:11 -0500

    add fzf to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,6 +10,22 @@ return  {
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 	end
 	},
+	{ -- If encountering errors, see telescope-fzf-native README for installation instructions
+		'nvim-telescope/telescope-fzf-native.nvim',
+
+		-- `build` is used to run some command when the plugin is installed/updated.
+		-- This is only run then, not every time Neovim starts up.
+		build = 'make',
+
+		-- `cond` is a condition used to determine whether this plugin should be
+		-- installed and loaded.
+		cond = function()
+			return vim.fn.executable 'make' == 1
+		end,
+		config = function()
+			require("telescope").load_extension("fzf")
+		end
+  },
 	{
 		'nvim-telescope/telescope-ui-select.nvim',
 		config = function()
