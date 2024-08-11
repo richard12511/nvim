452dfa3a7500f6610641e2c65ffada3f0d9c539a
Richard Schmidt <richardnew12511@gmail.com>
2024-08-11 10:52:05 -0500

    more options added

--- init.lua
+++ init.lua
@@ -1,10 +1,20 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 2        -- Number of spaces that a <Tab> in the file counts for
 vim.opt.shiftwidth = 2     -- Number of spaces to use for each step of (auto)indent
 vim.opt.scrolloff = 10
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.breakindent = true  
+vim.opt.undofile = true -- Save undo history
+vim.opt.ignorecase = true -- Case insensitive searching unless \C or one or more caps in search tearm
+vim.opt.smartcase = true
+vim.opt.signcolumn = 'yes'
+vim.keymap.set("i", "jj", "<Esc>") -- Easy way to get back to normal mode
+
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
