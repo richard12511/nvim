677bad289c5bf765059863ed947251a964507a77
Richard Schmidt <richardnew12511@gmail.com>
2024-08-11 14:43:15 -0500

    add better icons for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -22,5 +22,7 @@ return  {
 			})
 			require("telescope").load_extension("ui-select")
 		end
-	}
+	},
+	-- Useful for getting pretty icons, but requires a Nerd Font.
+  { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
 }
