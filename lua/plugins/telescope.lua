2fc828b8302433b19e24d54c8ed03b41b336f6a5
Richard Schmidt <richardnew12511@gmail.com>
2024-08-11 13:23:45 -0500

    update telescope init to have better ui-select window

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,5 @@
-return  { 
+return  {
+	{
 	'nvim-telescope/telescope.nvim',
 	tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
@@ -8,4 +9,18 @@ return  {
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 	end
+	},
+	{
+		'nvim-telescope/telescope-ui-select.nvim',
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown {}
+					}
+				}
+			})
+			require("telescope").load_extension("ui-select")
+		end
+	}
 }
