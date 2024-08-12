265b1b1a223236bcff0db3e0501ff445c9f98a99
Richard Schmidt <richardnew12511@gmail.com>
2024-08-12 16:57:40 -0500

    refactor keymaps and options to separate file

--- init.lua
+++ init.lua
@@ -1,46 +1,6 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-vim.g.have_nerd_font = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.tabstop = 2        -- Number of spaces that a <Tab> in the file counts for
-vim.opt.shiftwidth = 2     -- Number of spaces to use for each step of (auto)indent
-vim.opt.scrolloff = 10
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.breakindent = true
-vim.opt.undofile = true -- Save undo history
-vim.opt.ignorecase = true -- Case insensitive searching unless \C or one or more caps in search tearm
-vim.opt.smartcase = true
-vim.opt.cursorline = true -- Show which line your cursor is on
-vim.opt.signcolumn = 'yes'
+require("keymaps")
+require("options")
 
--- Set highlight on search, but allows you to remove highlight quickly by pressing escape
-vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
--- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
-vim.keymap.set("i", "jj", "<Esc>") -- Easy way to get back to normal mode
 
 
 -- Sync clipboard between OS and Neovim.
---
+++ lua/keymaps.lua
@@ -0,0 +1,26 @@
+
+-- Set highlight on search, but allows you to remove highlight quickly by pressing escape
+vim.opt.hlsearch = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+vim.keymap.set("i", "jj", "<Esc>") -- Easy way to get back to normal mode
