de46f09920484018711eb9f10da27cb36643176d
Richard Schmidt <richardnew12511@gmail.com>
2024-08-12 14:35:05 -0500

    add lsp keymaps

--- init.lua
+++ init.lua
@@ -66,6 +66,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     os.exit(1)
   end
 end
+---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,51 +1,67 @@
 return
 {
-		{
-			"williamboman/mason.nvim",
-			config = function()
-				require("mason").setup()
-			end
-		},
-		{
-			"williamboman/mason-lspconfig.nvim",
-			config = function()
-				require("mason-lspconfig").setup {
-						ensure_installed = { "lua_ls", "elp", "elixirls" },
-				}
-			end
-		},
-		{
-			"neovim/nvim-lspconfig",
-			config = function()
-				local lspconfig = require("lspconfig")
-				lspconfig.lua_ls.setup {
-					settings = {
-						Lua = {
-							runtime = {
-								-- Tell the language server which version of Lua you're using
-								-- (most likely LuaJIT in the case of Neovim)
-								version = 'LuaJIT',
-							},
-							diagnostics = {
-								-- Get the language server to recognize the `vim` global
-								globals = {
-									'vim',
-									'require'
-								},
-							},
-							workspace = {
-								-- Make the server aware of Neovim runtime files
-								library = vim.api.nvim_get_runtime_file("", true),
-							},
-							-- Do not send telemetry data containing a randomized but unique identifier
-							telemetry = {
-								enable = false,
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup {
+				ensure_installed = { "lua_ls", "elp", "elixirls" },
+			}
+		end
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup {
+				settings = {
+					Lua = {
+						runtime = {
+							-- Tell the language server which version of Lua you're using
+							-- (most likely LuaJIT in the case of Neovim)
+							version = 'LuaJIT',
+						},
+						diagnostics = {
+							-- Get the language server to recognize the `vim` global
+							globals = {
+								'vim',
+								'require'
 							},
 						},
+						workspace = {
+							-- Make the server aware of Neovim runtime files
+							library = vim.api.nvim_get_runtime_file("", true),
+						},
+						-- Do not send telemetry data containing a randomized but unique identifier
+						telemetry = {
+							enable = false,
+						},
 					},
-				}
+				},
+			}
 
-				vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-			end
-		}
+			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+			vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+			vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, {})
+			vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, {})
+			vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, {})
+			vim.keymap.set('n', '<leader>wl', function()
+				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+			end, {})
+			vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, {})
+			vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+			vim.keymap.set('n', 'v', vim.lsp.buf.code_action, {})
+			vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
+			vim.keymap.set('n', '<leader>f', function()
+				vim.lsp.buf.format { async = true }
+			end, {})
+		end
+	}
 }
