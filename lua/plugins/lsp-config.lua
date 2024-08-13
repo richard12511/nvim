949e6c38ad134e2b217589c2a617b4880d267d4e
Richard Schmidt <richardnew12511@gmail.com>
2024-08-13 15:25:45 -0500

    change code actions to match vscode

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,36 +1,35 @@
-return
-{
+return {
 	{
 		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup()
-		end
+		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
-			require("mason-lspconfig").setup {
+			require("mason-lspconfig").setup({
 				ensure_installed = { "lua_ls", "elp", "elixirls", "tsserver" },
-			}
-		end
+			})
+		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup {
+			lspconfig.lua_ls.setup({
 				settings = {
 					Lua = {
 						runtime = {
 							-- Tell the language server which version of Lua you're using
 							-- (most likely LuaJIT in the case of Neovim)
-							version = 'LuaJIT',
+							version = "LuaJIT",
 						},
 						diagnostics = {
 							-- Get the language server to recognize the `vim` global
 							globals = {
-								'vim',
-								'require'
+								"vim",
+								"require",
 							},
 						},
 						workspace = {
@@ -43,26 +42,26 @@ return
 						},
 					},
 				},
-			}
+			})
 			lspconfig.tsserver.setup({})
 
-			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-			vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-			vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, {})
-			vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, {})
-			vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, {})
-			vim.keymap.set('n', '<leader>wl', function()
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
+			vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, {})
+			vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, {})
+			vim.keymap.set("n", "<leader>wl", function()
 				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 			end, {})
-			vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, {})
-			vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
-			vim.keymap.set('n', 'v', vim.lsp.buf.code_action, {})
-			vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
-			vim.keymap.set('n', '<leader>f', function()
-				vim.lsp.buf.format { async = true }
+			vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, {})
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+			vim.keymap.set("n", "<C-.>", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>f", function()
+				vim.lsp.buf.format({ async = true })
 			end, {})
-		end
-	}
+		end,
+	},
 }
