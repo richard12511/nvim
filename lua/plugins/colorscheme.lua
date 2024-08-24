68ef30572c7bee2046e28e45dc0e882de9ade8c3
Richard Schmidt <richardnew12511@gmail.com>
2024-08-24 10:14:14 -0500

    switch back to cattpuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,19 +1,20 @@
-return {
-	"craftzdog/solarized-osaka.nvim",
-	lazy = true,
-	priority = 1000,
-	opts = function()
-		return {
-			transparent = true,
-		}
-	end,
-}
---return { 
---	"catppuccin/nvim", 
---	lazy = false,
---	name = "catppuccin",
+--return {
+--	"craftzdog/solarized-osaka.nvim",
+--	lazy = true,
 --	priority = 1000,
---	config = function()
---		vim.cmd.colorscheme "catppuccin"
---	end
+--	opts = function()
+--		return {
+--			transparent = true,
+--		}
+--	end,
 --}
+
+return {
+	"catppuccin/nvim",
+	lazy = false,
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		vim.cmd.colorscheme "catppuccin"
+	end
+}
