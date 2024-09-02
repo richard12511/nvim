98f03ce8ca4c4bd9bec941b41a8c8f45f37939a6
Richard Schmidt <richardnew12511@gmail.com>
2024-09-02 17:26:44 -0500

    add languages to treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,23 @@ return {
 	config = function()
 
 		local configs = require("nvim-treesitter.configs") configs.setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "eex", "heex", "javascript", "html" },
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"elixir",
+				"eex",
+				"heex",
+				"javascript",
+				"typescript",
+				"tsx",
+				"json",
+				"yaml",
+				"css",
+				"html",
+			},
 						sync_install = false,
 						highlight = { enable = true },
 						indent = { enable = true },
