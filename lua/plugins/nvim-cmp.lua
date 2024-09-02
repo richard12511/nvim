ef8d48c3b9886ac6847aca34fe4010fa1e06e9fd
Richard Schmidt <richardnew12511@gmail.com>
2024-09-02 17:49:06 -0500

    add more snippets for lua, but still need to get luasnip working

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -5,6 +5,14 @@ return {
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
+			-- {
+			-- 	"L3MON4D3/LuaSnip",
+			-- 	version = "v2.*",
+			-- 	build = "make install_jsregexp",
+			-- },
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+			"onsails/lspkind.nvim",
 		},
 		config = function()
 			local cmp = require("cmp")
