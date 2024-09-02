f7ba3f7f6e1f47ccbac9dbde075612aa61d8f1e9
Richard Schmidt <richardnew12511@gmail.com>
2024-09-02 17:22:51 -0500

    add config for treesj

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -3,6 +3,11 @@ return {
   keys = { '<space>m', '<space>j', '<space>s' },
   dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
   config = function()
-    require('treesj').setup({--[[ your config ]]})
+    require('treesj').setup({
+			vim.keymap.set('n', '<leader>m', require('treesj').toggle);
+			vim.keymap.set('n', '<leader>M', function()
+				require('treesj').toggle({ split = { recursive = true } })
+			end)
+		})
   end,
 }
